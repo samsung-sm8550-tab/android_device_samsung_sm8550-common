c20dcc0798a40f432a42dc0d0b18868f416b8d96
Ethan Chen <intervigil@gmail.com>
2018-12-01 13:53:00 -0800

    sm8550-common: Declare EUICC permission
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Co-authored-by: razorloves <razorloves@gmail.com>
    Change-Id: I323dce8b848b44e45ed21002950059e58719113c

--- common.mk
+++ common.mk
@@ -257,6 +257,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     OpenEUICC
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.mep.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.mep.xml \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
