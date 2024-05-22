fec3f9ef631acb1cc8eedf19001b171485ca0795
lostark13 <tushar9915@gmail.com>
2024-05-22 13:50:10 +0800

    sm8550-common: parts: Initialize parts with per-app refresh rate

--- common.mk
+++ common.mk
@@ -275,6 +275,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Parts
+PRODUCT_PACKAGES += \
+    SamParts
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
