bf1dec693751ab4ecf3526a1c9ab86a3dbb87355
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-05-13 16:28:27 +0200

    sm8550-common: Setup DeviceAsWebcam
    Kanged from: https://github.com/xiaomi-8550/android_device_xiaomi_sm8550-common/tree/vauxite/overlays/DeviceAsWebcamRes
    
    Change-Id: Ib319988b01034893cef1074683e6de99fc49c67b

--- common.mk
+++ common.mk
@@ -287,6 +287,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    DeviceAsWebcamResCommon \
     FrameworksResCommon \
     FrameworksResSamsung \
     FrameworksResTarget \
