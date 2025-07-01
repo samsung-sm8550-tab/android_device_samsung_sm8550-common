7e714fc7aa7d3e957a5d22768facdf0655317cb2
Bruno Martins <bgcngm@gmail.com>
2025-06-30 21:43:35 -0700

    sm8550-common: touch: Implement aidl touch Change-Id: I7f5e9004f81e5551dda899031c70cd5e59995920
    Change-Id: I9cc4a28733c1b3f7b721b96b323768d96d96ea2b

--- common.mk
+++ common.mk
@@ -230,7 +230,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.samsung-qcom.sm8550
+    vendor.lineage.livedisplay@2.0-service.samsung-qcom.sm8550 \
+    vendor.lineage.touch-service.samsung
 
 # Keymint
 PRODUCT_PACKAGES += \
